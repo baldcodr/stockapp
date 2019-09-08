98670aa7d7d9fd49c683cd5c3bd0e33cb716ee79
David Akuma <chukay360@gmail.com>
2019-09-08 16:39:27 +0100

    successfully pulls data from iexcloud API

--- app.js
+++ app.js
@@ -2,16 +2,49 @@ const express = require('express');
 const app = express();
 const exphbs  = require('express-handlebars');
 const path = require('path');
+const request = require('request');
 
 const PORT = process.env.PORT || 5000;
+
+//Create call API function
+function call_api(apiFINISHED){
+    request('https://cloud.iexapis.com/stable/stock/aapl/quote?token=Tpk_e4f71b7b5c7c4783ac5383e31f79a75f', {json: true}, (err, res, body) => {
+    if (err) {return console.log(err);}
+    if (res.statusCode === 200){
+        //console.log(body);
+        apiFINISHED(body);
+    };
+});
+
+};//End of call-api function
+
 
 //Set handlebars middleware
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
-//Set handlebars route
+//Set handlebars route hone
 app.get('/', function (req, res) {
-    res.render('home');
+    call_api(function(apiLOADED){
+            res.render('home',{
+            stock: apiLOADED
+        });
+    });
+});
+
+//Set handlebars route about
+app.get('/about', function (req, res) {
+    res.render('about');
+});
+
+//Set handlebars route all-stocks
+app.get('/all-stocks', function (req, res) {
+    res.render('all-stocks');
+});
+
+//Set handlebars route stock-details
+app.get('/stock-details', function (req, res) {
+    res.render('stock-details');
 });
 
 //Set static folder
